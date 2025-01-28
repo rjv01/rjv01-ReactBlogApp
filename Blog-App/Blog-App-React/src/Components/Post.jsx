8e986db06df388e70b9fcb69022bbd0e51dc8975
rjv01 <verma.rajshekhar1@gmail.com>
2025-01-28 16:40:49 +0530

    date format small

--- Blog-App/Blog-App-React/src/Components/Post.jsx
+++ Blog-App/Blog-App-React/src/Components/Post.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'
 
 export default function Post({post}) {
   return (
-    <div className='border border-white m-3 p-3 border w-[320px] shadow-2xl rounded-2xl text-xl gap-3 h-[130px]'>
+    <div className='border border-white m-3 p-3 w-[320px] shadow-2xl rounded-2xl text-xl gap-3 h-[130px]'>
         <Link to={`/post/${post.id}`} >
             <h2 className='hover:underline hover:text-2xl duration-200'>{post.title}</h2>
-            <p>{post.datetime}</p>
+            <p className='text-lg'>{post.datetime}</p>
         </Link>
         <p>{
             (post.body).length <= 25 ? 
